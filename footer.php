fa5f78e8e9567310203431147f5cf15c967cc1d2
moonstarring <vsambeam@gmail.com>
2025-03-10 04:11:56 +0800

    updated log in

--- footer.php
+++ footer.php
@@ -1,66 +1,40 @@
-<div class="text-center text-lg-start bg-body-tertiary text-muted border-top m-0">
-    <div class="container text-center text-md-start mt-5">
-        <div class="row mt-3">
-            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                <!-- Content -->
-                <h5 class="text-uppercase fw-bold mb-4">
-                    <a href="" class="rb text-decoration-none"><img src="images/brand/rb logo colored.png" class="me-1" alt="" width="50px" height="auto">Rentbox</a>
-                </h5>
-                <p>Rentbox is a peer to peer platform where you can borrow and share gadgets from other owners!<br>Rent Gadgets, Your Way</p>
-            </div>
-            <!-- Grid column -->
+<footer class="container-fluid border-top pb-5 bg-body m-0">
+    <div class="row m-5">
+        <div class="col">
+            <img class="mb-2" src="images/rb logo text colored.png" alt="" height="40px;">
+            <p class="mb-2">Rentbox is a company registered in the Philippines with Company Reg. No. CS2024123456</>
 
-            <!-- Grid column -->
-            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                <!-- Links -->
-                <h6 class="text-uppercase fw-bold mb-4">
-                    Our Socials
-                </h6>
-                <p><a href="#" class="me-4 text-reset"><i class="bi bi-facebook"></i></a></p>
-                <p><a href="#" class="me-4 text-reset"><i class="bi bi-twitter-x"></i></a></p>
-                <p><a href="#" class="me-4 text-reset"><i class="bi bi-linkedin"></i></a></p>
+            <div class="me-5 mb-3">
+                <div class="d-flex align-items-center justify-content-between me-5 gap-4 bg-dark p-2 rounded">
+                    <i class="bi bi-facebook text-white fs-6"></i>
+                    <i class="bi bi-youtube text-white fs-6"></i>
+                    <i class="bi bi-tiktok text-white fs-6"></i>
+                    <i class="bi bi-instagram text-white fs-6"></i>
+                    <i class="bi bi-linkedin text-white fs-6"></i>
+                </div>
             </div>
-            <!-- Grid column -->
-
-            <!-- Grid column -->
-            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                <!-- Links -->
-                <h6 class="text-uppercase fw-bold mb-4">
-                    Useful links
-                </h6>
-                <p>
-                    <a href="#" class="text-decoration-none text-reset">My Profile</a>
-                </p>
-                <p>
-                    <a href="#" class="text-decoration-none text-reset">Rentals</a>
-                </p>
-                <p>
-                    <a href="#" class="text-decoration-none text-reset">About Us</a>
-                </p>
-                <p>
-                    <a href="#" class="text-decoration-none text-reset">Customer Service</a>
-                </p>
-            </div>
-            <!-- Grid column -->
-
-            <!-- Grid column -->
-            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                <!-- Links -->
-                <h6 class="text-uppercase fw-bold mb-4">Contact Us</h6>
-                <p><i class="bi bi-house-door-fill me-2"></i>Zamboanga City, 7000, PH</p>
-                <p><i class="bi bi-envelope-fill me-2"></i>rentbox@gmail.com</p>
-                <p><i class="bi bi-telephone-fill me-2"></i>+63 930 219 4166</p>
-            </div>
-            <!-- Grid column -->
         </div>
-        <!-- Grid row -->
-    </div>
-    <!-- Section: Links  -->
 
-    <!-- Copyright -->
-    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
-        © 2025 Copyright:
-        <a class="text-reset fw-bolder" href="#">RENTBOX</a>
+        <div class="col d-flex flex-column gap-2">
+            <h5 class="fw-bold text-success mb-3">For Owners</h5>
+            <a href="" class="link-secondary link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover">Become an Owner</a>
+            <a href="" class="link-secondary link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover">Owner Dashboard</a>
+        </div>
+
+        <div class="col d-flex flex-column gap-2">
+            <h5 class="fw-bold text-success mb-3">For Renters</h5>
+            <a href="" class="link-secondary link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover">Create an Account</a>
+            <a href="" class="link-secondary link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover">Browse for Gadgets</a>
+            <a href="" class="link-secondary link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover">Start Renting</a>
+        </div>
+
+        <div class="col d-flex flex-column gap-2">
+            <h5 class="fw-bold text-success mb-3">Abount Rentbox</h5>
+            <a href="" class="link-secondary link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover">About Rentbox</a>
+            <a href="" class="link-secondary link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover">Help Center</a>
+            <a href="" class="link-secondary link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover">Terms and Conditions</a>
+            <a href="" class="link-secondary link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover">Privacy Policy</a>
+            <a href="" class="link-secondary link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover">Contact Us</a>
+        </div>
     </div>
-    <!-- Copyright -->
-</div>
+</footer>
--- login.php
+++ login.php
@@ -1,8 +1,8 @@
 <?php require_once 'head.php'; ?>
 <?php require_once 'navbar.php'; ?>
 
-<body class="container-fluid bg-dark-subtle">
-    <div class="row d-flex justify-content-center">
+<body class="container-fluid bg-dark-subtle m-0 p-0">
+    <div class="row d-flex justify-content-center m-0">
         <div class="card col-4 rounded-5 shadow-sm mb-5">
             <div class="card-body d-flex flex-column justify-content-center">
                 <h4 class="text-center mt-4 mb-4 fw-bold">Login</h4>
@@ -14,12 +14,12 @@
         
                 <form method="POST" action="" class="col">
                     <div class="form-floating mb-3 mx-3">
-                        <input type="email" name="email" class="form-control ps-4 rounded-5" id="floatingInput" placeholder="Email" required value="">
+                        <input type="email" name="email" class="form-control ps-4 rounded-5 focus-ring" id="floatingInput" placeholder="Email" required value="" style="--bs-focus-ring-color: rgba(var(--bs-success-rgb), .25)">
                         <label for="floatingInput" class="ps-4">Email</label>
                     </div>
         
                     <div class="form-floating mb-1 mx-3">
-                        <input type="password" name="password" class="form-control ps-4 rounded-5" id="floatingPassword" placeholder="Password" required>
+                        <input type="password" name="password" class="form-control ps-4 rounded-5 focus-ring" id="floatingPassword" placeholder="Password" required value="" style="--bs-focus-ring-color: rgba(var(--bs-success-rgb), .25)">
                         <label for="floatingPassword" class="ps-4">Password</label>
                     </div>
         
@@ -28,9 +28,9 @@
                     </div>
                     
                     <div class="d-flex flex-column justify-content-center align-items-center mb-4 mx-3 mb-3 gap-2">
-                        <button type="submit" class="btn btn-success shadow-sm fs-5 rounded-5 px-5 py-2">Login</button>
+                        <button type="submit" class="btn gradient-success shadow-sm rounded-5 px-5 py-2 ">Login</button>
                         <p class="text-secondary p-0 m-0">or</p>
-                        <a href="" class="btn btn-outline-success shadow-sm mb-3 fs-5 rounded-5 px-5 py-2">Create Account</a>
+                        <a href="signup.php" class="btn btn-outline-success shadow-sm mb-3 rounded-5 px-5 py-2">Create Account</a>
                     </div>
                 </form>
             </div>
@@ -38,3 +38,4 @@
     </div>
     <?php require_once 'footer.php'; ?>
 </body>
+</html>
