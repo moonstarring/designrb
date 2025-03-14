8a4c5dbce770bf607ee689a41f3aed1bbe00d877
moonstarring <vsambeam@gmail.com>
2025-03-14 21:54:51 +0800

    error page draft

---
+++ error.php
@@ -0,0 +1,10 @@
+<?php require_once 'head.php'; ?>
+<?php require_once 'navbar.php'; ?>
+<body>
+    <div class="container-fluid bg-dark-subtle d-flex justify-content-center align-items-center">
+        <h1 class="display-1"> 404 </h1>
+
+    </div>
+</body>
+<?php require_once 'navbar.php'; ?>
+<script src="vendor/bootstrap-5.3.3/dist/js/bootstrap.bundle.min.js"></script>
